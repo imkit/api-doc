4e9fdd6853d67c1f3303d7e8f7dfb90738cac204
Mu Jou <mujou0612@gmail.com>
2025-08-09 23:41:56 +0800

    Update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,9 +4,16 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span><strong>IMKIT Platform API</strong></span>,
   project: {
-    link: 'https://github.com/your-org/imkit-doc-website',
+    link: 'https://github.com/imkit/api-doc',
   },
-  docsRepositoryBase: 'https://github.com/your-org/imkit-doc-website',
+  docsRepositoryBase: 'https://github.com/imkit/api-doc',
+  // 自訂標題格式
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - IMKIT'
+    }
+  },
+  
   footer: {
     component: <span>© 2025 FUNTEK Inc. All rights reserved.</span>
   }
