0ec3ffd00bb334685061b5e85444878f9347f753
Mu Jou <mujou0612@gmail.com>
2025-08-09 23:46:37 +0800

    Fix theme & update index

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,16 +7,6 @@ const config: DocsThemeConfig = {
     link: 'https://github.com/imkit/api-doc',
   },
   docsRepositoryBase: 'https://github.com/imkit/api-doc',
-  // 自訂標題格式
-  useNextSeoProps() {
-    return {
-      titleTemplate: '%s - IMKIT'
-    }
-  },
-  
-  footer: {
-    component: <span>© 2025 FUNTEK Inc. All rights reserved.</span>
-  }
 }
 
 export default config
