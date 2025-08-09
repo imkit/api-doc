7ddf55a15275bd0e666e51efd14aa2d56e9ef8b8
Mu Jou <mujou0612@gmail.com>
2025-08-09 08:15:07 +0800

    update index

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -2,7 +2,25 @@ export default {
   index: {
     title: "基本介紹"
   },
+  auth: {
+    title: "驗證"
+  },
   user: {
     title: "用戶"
+  },
+  room: {
+    title: "聊天室"
+  },
+  message: {
+    title: "訊息"
+  },
+  moderation: {
+    title: "管理機制"
+  },
+  data_export: {
+    title: "數據匯出"
+  },
+  webhook: {
+    title: "Webhook"
   }
 }
