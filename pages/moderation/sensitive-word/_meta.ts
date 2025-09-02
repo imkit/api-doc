963a78cb9484c0b0f7fc4f40e7945f4e711667e1
sean <shonshon7@gmail.com>
2025-09-03 00:55:10 +0800

    [fix] update meta of sensitive-word

--- pages/moderation/sensitive-word/_meta.ts
+++ pages/moderation/sensitive-word/_meta.ts
@@ -1,6 +1,12 @@
 export default {
-  "blocklist": {
-    title: "封鎖用詞列表"
+  "get-sensitive-words": {
+    title: "取得敏感詞設定"
+  },
+  "update-sensitive-words": {
+    title: "更新敏感詞設定"
+  },
+  "delete-sensitive-words": {
+    title: "刪除敏感詞設定"
   }
 }
 
